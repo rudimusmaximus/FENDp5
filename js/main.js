ef394de3ecaaf6743abf7b4998d75748afc8cc08
Raul Flores, Jr. MBA "rudy <rudy@redcrowconsulting.com>
2018-11-05 10:57:36 -0600

    feat: incorporate submission 1 code review
     - edited h levels for better semantics
    
     - added alt labels
    
     - edited ARIA-roles and labels

--- js/main.js
+++ js/main.js
@@ -171,9 +171,10 @@ let createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + ' restaurant image.';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
